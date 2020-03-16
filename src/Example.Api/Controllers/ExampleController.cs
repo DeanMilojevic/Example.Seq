a4dd8195d15d67638ce0f82f2016688fd19b4d0f
Dean Milojevic <milojevicdean@gmail.com>
2020-03-16 18:08:39 +0100

    Created docker-compose and added required dependencies to the project

---
+++ src/Example.Api/Controllers/ExampleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Example.Api.Controllers
+{
+    [ApiController]
+    [Route("api/example")]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ILogger<ExampleController> _logger;
+
+        public ExampleController(ILogger<ExampleController> logger) => _logger = logger;
+
+        [HttpGet("/info")]
+        public IActionResult Info()
+        {
+            _logger.LogInformation("This is a information in the seq!");
+            return Ok();
+        }
+        
+        [HttpGet("/warning")]
+        public IActionResult Warning()
+        {
+            _logger.LogWarning("This is a warning in the seq!");
+            return Ok();
+        }
+        
+        [HttpGet("/critical")]
+        public IActionResult Critical()
+        {
+            _logger.LogCritical("This is a critical in the seq!");
+            return Ok();
+        }
+        
+        [HttpGet("/error")]
+        public IActionResult Error()
+        {
+            _logger.LogError("This is a error in the seq!");
+            return Ok();
+        }
+    }
+}
